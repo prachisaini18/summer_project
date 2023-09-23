ef2518c99846717b7ebc499f5a180db8da929d30
prachisaini18 <143344347+prachisaini18@users.noreply.github.com>
2023-09-23 15:17:25 +0530

    Create docker.py

---
+++ docker.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+print("Content-type: text/html")
+print()
+
+import subprocess
+import cgi
+
+form = cgi.FieldStorage()
+command= form.getvalue('cmd')
+output=subprocess.getoutput(command)
+
+print(output)
