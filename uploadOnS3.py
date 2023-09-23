6286bd68b85bbeb9a95c6751c6f931ad6c1b5896
prachisaini18 <143344347+prachisaini18@users.noreply.github.com>
2023-09-23 15:28:01 +0530

    Update uploadOnS3.py

--- uploadOnS3.py
+++ uploadOnS3.py
@@ -2,12 +2,15 @@
 print("Content-type: text/html")
 print()
 
+import cgitb
 import cgi
 import boto3
+from botocore.exceptions import NoCredentialsError
+cgitb.enable()
 
 form=cgi.FieldStorage()
-bucket_name= form.getvalue("bucket_name")
 myfilename = form["file"]
+bucket_name= form.getvalue("bucket_name")
 
 fh=myfilename.file
 filedata =fh.read()
@@ -16,12 +19,15 @@ yourfile = myfilename.filename
 fh = open("uploads/"+ yourfile, "wb")
 fh.write(filedata)
 
-def upload_to_s3(file_item, bucket_name, s3_filename):
-    s3= boto3.resource('s3', aws_access_key_id="AKIAZMFRY42V3XGI6F7P", aws_secret_access_key="PF8mVLJ7H871SHIBRwbsjr+kPYlGlplGbYpKMPWi", region_name="ap-south-1")
+print(bucket_name)
+region="ap-south-1"
 
+def upload_to_s3(file_item, bucket_name, s3_filename):
+    s3= boto3.client('s3', aws_access_key_id="AKIAZMFRY42V3XGI6F7P", aws_secret_access_key="PF8mVLJ7H871SHIBRwbsjr+kPYlGlplGbYpKMPWi", region_name="ap-south-1")
     try:
         print(file_item)
-        s3.Bucket(bucket_name).upload_file(yourfile, s3_object_key='newimg.jpg') 
+        
+        s3.upload_file("prachii.jpg", bucket_name, "newimg.jpg")
         return True
     except NoCredentialsError:
         print("AWS credentials not found.")
@@ -31,7 +37,7 @@ def upload_to_s3(file_item, bucket_name, s3_filename):
         return False
 
 if upload_to_s3(fh, bucket_name, myfilename.filename):
-    print(f"File uploaded successfully to S3 bucket '{bucket_name}' as '{s3_filename}'.")
+    print(f"File uploaded successfully to S3 bucket '{bucket_name}' ")
 else:
     print("File not uploaded")
 fh.close()
